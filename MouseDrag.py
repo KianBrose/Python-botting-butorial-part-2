bbed111e9ed0324a0e7dcf73ff50933dc4b57002
17kibr <17kibr@stockholmscience.se>
2020-05-27 19:02:18 +0200

    Update MouseDrag.py

--- MouseDrag.py
+++ MouseDrag.py
@@ -4,10 +4,10 @@ import win32api, win32con
 
 time.sleep(2)
 
-win32api.SetCursorPos((847,605))
+win32api.SetCursorPos((500,500))
 time.sleep(0.1)
 win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
 time.sleep(0.1)
-win32api.SetCursorPos((847,405))
+win32api.SetCursorPos((500,300))
 time.sleep(0.1)
 win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
