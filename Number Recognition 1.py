0be1a9cd15b6c7637772dd1b3297d9507c687ad4
17kibr <17kibr@stockholmscience.se>
2020-05-27 10:06:31 +0200

    Added the first number recognition file with comments and file that will be used for the test

--- Number Recognition 1.py
+++ Number Recognition 1.py
@@ -1,11 +1,24 @@
-from pyautogui import *
 import pyautogui
-import time
-import keyboard
-import random
-import win32api, win32con
+from PIL import Image
+from pytesseract import *
+#Note: You have to change the path below to your tesseract.exe location
+pytesseract.tesseract_cmd = r'C:\Users\Antec\AppData\Local\Tesseract-OCR\tesseract.exe'
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+#This line stores the image texttoconvert.png inside the img variable
+img  = Image.open("texttoconvert.png")
+
+#Note: You can use pyautogui to fetch the screenshot instead of fetching it from a file
+#img = pyautogui.screenshot(region=(10,10,10,10))
+
+#This line uses pytesseract's image_to_string function to convert the image stored in the variable (img) into a string (text) that will be stored in the (output variable)
+output = pytesseract.image_to_string(img)
+
+#We can now print out the output
+print(output)
+
+#output = coords.split(',')
+#print(output[0])
+#print(output[1])
+#print(output)
+#print(len(output))
+
---
+++ Practical Application.py
@@ -0,0 +1,11 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
